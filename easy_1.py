594d144a0c3f034f5fef9ff3cc11a8533f57d30b
James <valeness84@gmail.com>
2015-02-11 19:38:31 -0600

    Easy Challenge #1 is done. Will add further, needlessly complex, features later.

--- easy_1.py
+++ easy_1.py
@@ -3,6 +3,7 @@ Program that aks the user their name, age, and reddit handle.
 Then read it back to them
 Saves the information to JSON for easy retrieval
 """
+import json
 
 print("Enter Your Name Age and Reddit Username in the following format")
 print("Name Age Reddit")
@@ -18,3 +19,15 @@ Your name is <<{0}>> and you are <<{1}>> years old.
 You are known on reddit as <<{2}>>""".format(nar_list[0], nar_list[1], nar_list[2])
 print(message)
 
+print("Do you want to save this information? y/n")
+print("This will overwrite the previous data")
+choice = raw_input(">>")
+
+if 'y' in choice:
+	file_path = 'files/easy_1_save.json'
+	with open(file_path, 'w') as f:
+		save_data = json.dumps(nar_list)
+		f.write(save_data)
+		print("Successfully Saved to <<{0}>>".format(file_path))
+else:
+	print("Data was not saved")
