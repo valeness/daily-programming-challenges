2514b9b5831fd45100d93a6d782972df29e31e12
James <valeness84@gmail.com>
2015-02-12 22:59:38 -0600

    Program works one time through. Next we add an option to repeat.

--- difficult_1.py
+++ difficult_1.py
@@ -5,8 +5,43 @@ The use will then respond with "higher" or "lower" until the computer gets it ri
 """
 from random import randrange
 
-limit = 100
-base = 0
+running = True
 
-guess = randrange(base, limit)
-print(guess)
+directions = """
+Think of a number from 1 - 100. I will try to guess it!
+Tell me if it is too high or too low by typing 
+an 'H/h' for high, and a 'L/l' for low
+"""
+
+print(directions)
+
+base = 0
+limit = 100
+
+def get_guess():
+	guess = randrange(base, limit)
+	print("Guess: {0}".format(guess))
+	return guess
+
+def too_low():
+	base = guess
+	return base
+
+def too_high():
+	limit = guess
+	return limit
+
+# Get the Initial Guess
+guess = get_guess()
+
+while running:
+	command = raw_input('>>').lower()
+
+	if 'h' in command:
+		limit = too_high()
+		guess = get_guess()
+	elif 'l' in command:
+		base = too_low()
+		guess = get_guess()
+	elif 'y' in command:
+		print("Yay! I guessed right!")
